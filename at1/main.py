9f31fa7eb40695751af2c09f0095837dd1aa99c1
VectorBatesta <vitorbatista@alunos.utfpr.edu.br>
2024-04-13 08:58:14 -0300

    set to file instead of printing

--- at1/main.py
+++ at1/main.py
@@ -4,13 +4,13 @@ import time
 def ordena(vetor, tam, escolhateste):
     match escolhateste:
         case 0: #Melhor caso: vetor já ordenado em ordem crescente;
-            print(f'Melhor caso: ')
+            arq.write(f'Melhor caso: ')
             vetor = [i for i in range(tam)]
         case 1: #Caso médio: vetor aleatório;
-            print(f'Caso médio: ')
+            arq.write(f'Caso médio: ')
             vetor = np.random.randint(0, tam, tam)
         case 2: #Pior caso: vetor já ordenado em ordem decrescente;
-            print(f'Pior caso: ')
+            arq.write(f'Pior caso: ')
             vetor = [i for i in reversed(range(tam))]
 
     for iteracao in range(10):
@@ -29,37 +29,38 @@ def ordena(vetor, tam, escolhateste):
         ####
         end = time.time()
         temposGastos[iteracao] = end - begin
-        print(f' iteração {iteracao} terminada, gasto: {temposGastos[iteracao]:.5f} segundos;')
+        arq.write(f' iteração {iteracao} terminada, gasto: {temposGastos[iteracao]:.5f} segundos;')
 
-    print(f'  gastou-se {sum(temposGastos):.5f} segundos no total;')
-    print(f'  gastou-se {np.mean(temposGastos):.5f} segundos em média;')
+    arq.write(f'  gastou-se {sum(temposGastos):.5f} segundos no total;')
+    arq.write(f'  gastou-se {np.mean(temposGastos):.5f} segundos em média;')
     return
 
 
 if __name__ == '__main__':
+    arq = open("out.txt", "a")
     tempoinicialprograma = time.time()
 
     for escolhatam in range(5):
-        print("\n\n")
+        arq.write("\n\n")
         match escolhatam:
             case 0:
                 tam = 50000
-                print("[[[[[[[[[[[[[[[ 50 mil elementos ]]]]]]]]]]]]]]]")
+                arq.write("[[[[[[[[[[[[[[[ 50 mil elementos ]]]]]]]]]]]]]]]")
             case 1:
                 tam = 100000
-                print("[[[[[[[[[[[[[[[ 100 mil elementos ]]]]]]]]]]]]]]]")
+                arq.write("[[[[[[[[[[[[[[[ 100 mil elementos ]]]]]]]]]]]]]]]")
             case 2:
                 tam = 150000
-                print("[[[[[[[[[[[[[[[ 150 mil elementos ]]]]]]]]]]]]]]]")
+                arq.write("[[[[[[[[[[[[[[[ 150 mil elementos ]]]]]]]]]]]]]]]")
             case 3:
                 tam = 200000
-                print("[[[[[[[[[[[[[[[ 200 mil elementos ]]]]]]]]]]]]]]]")
+                arq.write("[[[[[[[[[[[[[[[ 200 mil elementos ]]]]]]]]]]]]]]]")
             case 4:
                 tam = 250000
-                print("[[[[[[[[[[[[[[[ 250 mil elementos ]]]]]]]]]]]]]]]")
+                arq.write("[[[[[[[[[[[[[[[ 250 mil elementos ]]]]]]]]]]]]]]]")
             case 5:
                 tam = 300000
-                print("[[[[[[[[[[[[[[[ 300 mil elementos ]]]]]]]]]]]]]]]")
+                arq.write("[[[[[[[[[[[[[[[ 300 mil elementos ]]]]]]]]]]]]]]]")
 
         #alocacao de ram
         vetor = [0 for _ in range(tam)]
@@ -70,6 +71,7 @@ if __name__ == '__main__':
 
     tempofinalprograma = time.time()
     tempoUtilizadoprograma = tempofinalprograma - tempoinicialprograma
-    print(f"""\n##########################
-          Tempo gasto total do programa: {tempoUtilizadoprograma:.4f}.
-          ##########################""")
+    arq.write(f"""\n####################################################
+                    Tempo gasto total do programa: {tempoUtilizadoprograma:.4f}.
+                    ####################################################""")
+    arq.close()
