0cbf870ea802169809116d5ead058f106d2eb0fb
VectorBatesta <vitorbatista@alunos.utfpr.edu.br>
2024-04-13 21:39:16 -0300

    tam = tam /10

--- at1/main.py
+++ at1/main.py
@@ -43,27 +43,29 @@ def ordena(vetor, tam, escolhateste):
 if __name__ == '__main__':
     tempoinicialprograma = time.time()
 
-    for escolhatam in range(5):
+    for escolhatam in range(
+        #6, <= for testing 6
+        6):
         printadorArquivasso("\n\n")
         match escolhatam:
             case 0:
                 tam = 50000
-                printadorArquivasso(f'\n[[[[[[[[[[[[[[[ 50 mil elementos ]]]]]]]]]]]]]]]')
             case 1:
                 tam = 100000
-                printadorArquivasso(f'\n[[[[[[[[[[[[[[[ 100 mil elementos ]]]]]]]]]]]]]]]')
             case 2:
                 tam = 150000
-                printadorArquivasso(f'\n[[[[[[[[[[[[[[[ 150 mil elementos ]]]]]]]]]]]]]]]')
             case 3:
                 tam = 200000
-                printadorArquivasso(f'\n[[[[[[[[[[[[[[[ 200 mil elementos ]]]]]]]]]]]]]]]')
             case 4:
                 tam = 250000
-                printadorArquivasso(f'\n[[[[[[[[[[[[[[[ 250 mil elementos ]]]]]]]]]]]]]]]')
             case 5:
                 tam = 300000
-                printadorArquivasso(f'\n[[[[[[[[[[[[[[[ 300 mil elementos ]]]]]]]]]]]]]]]')
+        
+        #if processador.qualidade = ruim:
+        tam = int(tam / 10)
+        #comentar /\ caso pc for bom
+
+        printadorArquivasso(f'\n[[[[[[[[[[[[[[[ {tam} mil elementos ]]]]]]]]]]]]]]]')
 
         #alocacao de ram
         vetor = [0 for _ in range(tam)]
